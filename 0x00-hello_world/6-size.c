3c17544d8d092fc4757a2b1a7cfa777ff43813b5
muchokiesther <muchokiesther8@gmail.com>
2023-02-16 13:56:01 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ printf("Size of a int: %zu byte(s)", sizeof(int));
 printf("Size of a long int: %zu byts(s)", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
 printf("Size of a float: %zu byte(s)", sizeof(float));
-return (0);
+turn (0);
 }
