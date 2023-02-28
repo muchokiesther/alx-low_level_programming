24dcc39f1ed9ea2da742c35c2714dd37f4dd4218
muchokiesther <muchokiesther8@gmail.com>
2023-02-28 06:50:09 -0800

    task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,5 +13,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#define main
 #endif
